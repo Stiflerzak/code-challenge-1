9488ba55704f2a38eeca3d6f53488462195e52d7
Stiflerzak <hilarymwendia97@gmail.com>
2022-11-28 19:52:10 +0300

    code challenge

--- README.md
+++ README.md
@@ -3,7 +3,15 @@
 
 # Description
 This is inclusive of a  student grading miniproject, speed detector and net salary calculator.
+-student grading miniproject
+This project takes in the score and computes the grade.
 
+-speed_detector
+This takes in the speed of a given vehicle and determine whether speed is within the required range.
+It responds to the user.
+
+-Net salary calculator
+This project prompts user to input salary which is computed to determine the net salary.
 
 # Getting Started
 In order for you to use the content on this repo ensure you have the following:
@@ -23,7 +31,8 @@ To use this repo on your machine requires some simple steps
 
         git clone git@github.com:Stiflerzak/code-challenge-1.git
 
-- open the cli terminal and type the name of the .js 
+-Vscode is recommended for best experience.
+-This project runs on the browser. One should install live server extention. 
 
 
 # Authors
--- speed-detector.js
+++ speed-detector.js
@@ -1,57 +1,17 @@
-//this function accepts speed argument and computes it.
-function Speed_detector(speed) {
- 
-  //switch case to determine demerit for each speed range
-    switch(true) {
-  case (speed >=1 && speed <= 70):
-  demerit = 'OK';
-   break;
-  case (speed <= 75 && speed > 70):
-   demerit = 'One point added';
-    break;
-  case (speed <= 80 && speed >75):
-   demerit = 'two points added';
-    break;
-  case (speed <= 85 && speed > 80):
-   demerit = 'three points added';
-    break;
-  case (speed <= 90 && speed > 85):
-   demerit = 'four points added';
-   break;
-   case (speed <= 95 && speed > 90):
-   demerit = 'five points added';
-   break;
-   case (speed <= 100 && speed > 95):
-   demerit = 'six points added';
-   break;
-   case (speed <= 105 && speed > 100):
-   demerit = 'seven points added';
-   break;
-   case (speed <= 110 && speed > 105):
-   demerit = 'eight points added';
-   break;
-   case (speed <= 115 && speed > 110):
-   demerit = 'nine points added';
-   break;
-   case (speed <= 120 && speed > 115):
-   demerit = 'ten points added';
-   break;
-   case (speed <= 125 && speed > 120):
-   demerit = 'eleven points added';
-   break;
-  
-  case (speed >= 125):
-   demerit = 'License suspended';
-   break;
-  default:
-  return 'License suspended';
-  }
-  }
-  
-  //prompts the user to enter speed
-  let speed = prompt("Enter your Speed");
-  
-  
-  //print out the demerit 
-  console.log(Speed_detector(speed));
-  
+//Declaration of variables
+let speedLimit = 70;
+let pointcount = 0;
+//This prompts user to key in the speed for computation.
+let speed = prompt("Enter Your speed");
+
+//if statement that computes the condition of speed. If condition is true, "okay" alert is displayed.
+if(speed <= 70) {
+    alert("Speed is OK")
+}
+else{
+    pointcount = Math.floor((speed - speedLimit) / 5);
+    //ternary expression to determine the points
+    let output = pointcount > 12 ? "License suspended": "Pointcount = "+pointcount
+    alert(output);
+
+}
