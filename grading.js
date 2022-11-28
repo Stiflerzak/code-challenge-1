a0f7161c73d1f6ce2c0318bd726c08454e57c677
Stiflerzak <hilarymwendia97@gmail.com>
2022-11-28 19:08:58 +0300

    code challenge

--- grading.js
+++ grading.js
@@ -1,36 +1,32 @@
 //function that computes the score of a given student
 function Grade_generator(score) {
- 
+  if(score > 100 || score < 0){
+    return `${grade} is invalid!`;
+}
+else{
+    if (score > 79) {
+        return  "A";
+    }
+    else if (score > 59 && score <= 79) {
+        return  "B";
+    }
+    else if (score > 49 && score <= 59){
+        return  "C";
+    }
+    else if (score > 39 && score <= 49){
+        return  "D";
+    }
+    else{
+        return  "E";
+    }
+}
+
+} 
   
-    switch(true) {
-  case (score <= 100 && score >= 79):
-  grade = 'A';
-   break;
-  case (score <= 78 && score >= 60):
-   grade = 'B';
-    break;
-  case (score <= 59 && score >= 49):
-   grade = 'C';
-    break;
-  case (score <= 48 && score >= 40):
-   grade = 'D';
-    break;
-  case (score <= 39 && score >= 0):
-   grade = 'E';
-   break;
   
-  case (score > 100 && score < 0):
-   grade = 'Score is Invalid';
-   break;
-  default:
-  return 'Score is Invalid';
-  }
-  }
-  
-  //prompts the user to enter marks scored
-  let score = prompt("Enter your Score");
-  let grade= Grade_generator(score)
-  alert(`You grade: ${grade}`);
+  let grade = prompt("Enter your Score");
+  let score= Grade_generator(grade)
+  alert(`You grade: ${score}`);
   
   //print out the grade 
   console.log(Grade_generator(score));
--- speed-detector.js
+++ speed-detector.js
@@ -1,73 +1,52 @@
 //this function accepts speed argument and computes it.
 function Speed_detector(speed) {
-  if(grade > 100 && grade < 0){
-    return `${grade} is invalid!`;
-}
-else{
-    if (grade > 79) {
-        return  "A";
-    }
-    else if (grade > 59 && grade <= 79) {
-        return  "B";
-    }
-    else if (grade > 49 && grade <= 59){
-        return  "C";
-    }
-    else if (grade > 39 && grade <= 49){
-        return  "D";
-    }
-    else{
-        return  "E";
-    }
-}
-
-} 
+ 
   //switch case to determine demerit for each speed range
-  //   switch(true) {
-  // case (speed >=1 && speed <= 70):
-  // demerit = 'OK';
-  //  break;
-  // case (speed <= 75 && speed > 70):
-  //  demerit = 'One point added';
-  //   break;
-  // case (speed <= 80 && speed >75):
-  //  demerit = 'two points added';
-  //   break;
-  // case (speed <= 85 && speed > 80):
-  //  demerit = 'three points added';
-  //   break;
-  // case (speed <= 90 && speed > 85):
-  //  demerit = 'four points added';
-  //  break;
-  //  case (speed <= 95 && speed > 90):
-  //  demerit = 'five points added';
-  //  break;
-  //  case (speed <= 100 && speed > 95):
-  //  demerit = 'six points added';
-  //  break;
-  //  case (speed <= 105 && speed > 100):
-  //  demerit = 'seven points added';
-  //  break;
-  //  case (speed <= 110 && speed > 105):
-  //  demerit = 'eight points added';
-  //  break;
-  //  case (speed <= 115 && speed > 110):
-  //  demerit = 'nine points added';
-  //  break;
-  //  case (speed <= 120 && speed > 115):
-  //  demerit = 'ten points added';
-  //  break;
-  //  case (speed <= 125 && speed > 120):
-  //  demerit = 'eleven points added';
-  //  break;
+    switch(true) {
+  case (speed >=1 && speed <= 70):
+  demerit = 'OK';
+   break;
+  case (speed <= 75 && speed > 70):
+   demerit = 'One point added';
+    break;
+  case (speed <= 80 && speed >75):
+   demerit = 'two points added';
+    break;
+  case (speed <= 85 && speed > 80):
+   demerit = 'three points added';
+    break;
+  case (speed <= 90 && speed > 85):
+   demerit = 'four points added';
+   break;
+   case (speed <= 95 && speed > 90):
+   demerit = 'five points added';
+   break;
+   case (speed <= 100 && speed > 95):
+   demerit = 'six points added';
+   break;
+   case (speed <= 105 && speed > 100):
+   demerit = 'seven points added';
+   break;
+   case (speed <= 110 && speed > 105):
+   demerit = 'eight points added';
+   break;
+   case (speed <= 115 && speed > 110):
+   demerit = 'nine points added';
+   break;
+   case (speed <= 120 && speed > 115):
+   demerit = 'ten points added';
+   break;
+   case (speed <= 125 && speed > 120):
+   demerit = 'eleven points added';
+   break;
   
-  // case (speed >= 125):
-  //  demerit = 'License suspended';
-  //  break;
-  // default:
-  // return 'License suspended';
-  // }
-  // }
+  case (speed >= 125):
+   demerit = 'License suspended';
+   break;
+  default:
+  return 'License suspended';
+  }
+  }
   
   //prompts the user to enter speed
   let speed = prompt("Enter your Speed");
